73edfd25e3b47669507c8f693daa3a536586c827
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-11 22:35:54 +0530

    Created Navbar.tsx and implemented shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,16 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+// import { Button } from "@/components/ui/button";
+// import {
+//   SignedIn,
+//   SignedOut,
+//   SignInButton,
+//   SignUpButton,
+//   UserButton,
+// } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign In
-            </button>
-          </SignInButton>
-          <SignUpButton mode="modal">
-            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <h1>HomePage</h1>
     </div>
   );
 }
