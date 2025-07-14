0be16b6c68d6133e58e884d9a128a5e0106f2ce8
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-14 20:56:51 +0530

    Started with post creating component in home page and made getDbUserId action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,19 @@
 //   UserButton,
 // } from "@clerk/nextjs";
 
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
