6ab4b1661c215362e7391d6d3cb1d5eda5d76420
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-16 16:21:54 +0530

    getPosts and Toggle Like Function created and handleLike Function in PostCard  component created

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -46,7 +46,7 @@ function CreatePost() {
             </Avatar>
             <Textarea
               placeholder="What's on your mind?"
-              className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
+              className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-3 text-base"
               value={content}
               onChange={(e) => setContent(e.target.value)}
               disabled={isPosting}
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { getPosts, toggleLike } from "@/actions/post.actions";
+// import { Post } from '@/generated/prisma'
+import { useUser } from "@clerk/nextjs";
+import React, { useState } from "react";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+
+export default function PostCard({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCOmmenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId));
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+  const handleLike = async () =>{
+    if(isLiking) return
+    try {
+      setIsLiking(true);
+      setHasLiked(prev => !prev);
+      setOptimisticLikes(prev => prev + (hasLiked ? -1 : 1))
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some(like => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  }
+
+  const handleAddComment = async () =>{}
+
+  const handleDeletePost = async () =>{}
+
+  return <div>Postcard</div>;
+}
