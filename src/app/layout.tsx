0ef29429de8be0d2fa022f7a88079c4d52097a6a
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-19 16:52:02 +0530

    New Images and Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Socially",
+  title: "SocialSphere",
   description: "Modern Social Media Application powered by Next.js",
 };
 
